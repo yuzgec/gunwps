3f828b810235e88aad0db73928f15c93abc54aad
yuzgec <olcayy@gmail.com>
2023-09-13 17:44:00 +0300

    Fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                             @csrf
 
                             <div class="row mb-2">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Email Adresiniz</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Email Address</label>
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                     @error('email')
@@ -22,7 +22,7 @@
                             </div>
 
                             <div class="row">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Parolanız</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                     @error('password')
@@ -39,7 +39,7 @@
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            &nbsp;Beni Hatırla
+                                            &nbsp;Remember Me
                                         </label>
                                     </div>
                                 </div>
@@ -48,14 +48,14 @@
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Giriş Yap
+                                        Login
                                     </button>
 
-                                    @if (Route::has('password.request'))
+                                  {{--  @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
                                             Parolamı Unuttum
                                         </a>
-                                    @endif
+                                    @endif--}}
                                 </div>
                             </div>
                         </form>
